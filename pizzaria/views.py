d684ae5d572a64f1a9732966c1217ec55b95e405
john-emerson14 <john.carv.sousa@gmail.com>
2019-11-21 20:19:00 -0200

    feat: add all views and urls for serializers

--- pizzaria/views.py
+++ pizzaria/views.py
@@ -13,7 +13,13 @@ class ApiRoot(GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return Response({
-            "address": reverse(AddressList.name, request=request)
+            "address": reverse(AddressList.name, request=request),
+            "client": reverse(ClientList.name, request=request),
+            "manager": reverse(ManagerList.name, request=request),
+            "employee": reverse(EmployeeList.name, request=request),
+            "progress": reverse(ProgressList.name, request=request),
+            "pizza": reverse(PizzaList.name, request=request),
+            "demand": reverse(DemandList.name, request=request),
         }, status=status.HTTP_200_OK)
 
 
@@ -27,3 +33,75 @@ class AddressDetail(RetrieveUpdateDestroyAPIView):
     name = 'address-detail'
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+
+class ClientList(ListCreateAPIView):
+    name = 'client-list'
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ClientDetail(RetrieveUpdateDestroyAPIView):
+    name = 'client-detail'
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class ManagerList(ListCreateAPIView):
+    name = 'manager-list'
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+
+class ManagerDetail(RetrieveUpdateDestroyAPIView):
+    name = 'manager-detail'
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+
+class EmployeeList(ListCreateAPIView):
+    name = 'employee-list'
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeDetail(RetrieveUpdateDestroyAPIView):
+    name = 'employee-detail'
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class ProgressList(ListCreateAPIView):
+    name = 'progress-list'
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
+
+
+class ProgressDetail(RetrieveUpdateDestroyAPIView):
+    name = 'progress-detail'
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
+
+
+class PizzaList(ListCreateAPIView):
+    name = 'pizza-list'
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class PizzaDetail(RetrieveUpdateDestroyAPIView):
+    name = 'pizza-detail'
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class DemandList(ListCreateAPIView):
+    name = 'demand-list'
+    queryset = Demand.objects.all()
+    serializer_class = DemandSerializer
+
+
+class DemandDetail(RetrieveUpdateDestroyAPIView):
+    name = 'demand-detail'
+    queryset = Demand.objects.all()
+    serializer_class = DemandSerializer
