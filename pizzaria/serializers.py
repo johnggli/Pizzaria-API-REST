630776306e9a88a4e0fc728072e0cb9940b03e0f
john-emerson14 <john.carv.sousa@gmail.com>
2019-11-21 19:59:56 -0200

    refactor: add serializers

--- pizzaria/serializers.py
+++ pizzaria/serializers.py
@@ -3,7 +3,42 @@ from .models import *
 
 
 class AddressSerializer(ModelSerializer):
-    
     class Meta:
         model = Address
+        fields = '__all__'
+
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class ManagerSerializer(ModelSerializer):
+    class Meta:
+        model = Manager
+        fields = '__all__'
+
+
+class EmployeeSerializer(ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class ProgressSerializer(ModelSerializer):
+    class Meta:
+        model = Progress
+        fields = '__all__'
+
+
+class PizzaSerializer(ModelSerializer):
+    class Meta:
+        model = Pizza
+        fields = '__all__'
+
+
+class DemandSerializer(ModelSerializer):
+    class Meta:
+        model = Demand
         fields = '__all__'
