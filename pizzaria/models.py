ad2d31f220be101823da94e9100b88de6888b42c
john-emerson14 <john.carv.sousa@gmail.com>
2019-11-20 22:26:03 -0200

    refactor: change models

--- pizzaria/models.py
+++ pizzaria/models.py
@@ -1,40 +1,47 @@
 from django.db import models
 
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     suite = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     zipcode = models.CharField(max_length=255)
 
+
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=12)
-    address = models.ForeignKey(Address, models.CASCADE)
+    address = models.OneToOneField(Address, models.CASCADE, related_name='client')
+
 
 class Manager(models.Model):
     name = models.CharField(max_length=255)
-    cpf = models.IntegerField(max_length=11)
+    cpf = models.CharField(max_length=11)
     salary = models.FloatField()
     email = models.EmailField()
 
+
 class Employee(models.Model):
     name = models.CharField(max_length=255)
-    cpf = models.IntegerField(max_length=11)
+    cpf = models.CharField(max_length=11)
     salary = models.FloatField()
-    manager = models.ForeignKey(Manager, models.CASCADE)
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='employers')
+
+
+class Progress(models.Model):
+    name = models.CharField(max_length=255)
+
 
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.FloatField()
 
-class Progress(models.Model):
-    name = models.CharField(max_length=255)
 
 class Demand(models.Model):
-    date = models.DateField()
-    client = models.ForeignKey(Client, models.CASCADE)
-    employee = models.ForeignKey(Employee, models.CASCADE)
-    pizza = models.ForeignKey(Pizza, models.CASCADE)
-    progress = models.ForeignKey(Progress, models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    client = models.OneToOneField(Client, models.CASCADE)
+    employee = models.OneToOneField(Employee, models.CASCADE)
+    pizza = models.OneToOneField(Pizza, models.CASCADE)
+    progress = models.OneToOneField(Progress, models.CASCADE)
